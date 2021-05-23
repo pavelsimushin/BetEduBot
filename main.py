d739a2bbf22a0d7e72dd9ea5e5f8b315690e11cc
pavelsimushin <78613095+pavelsimushin@users.noreply.github.com>
2021-05-23 11:40:39 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,61 @@
+import telebot
+import db
+from bs4 import BeautifulSoup
+import requests
+# import telegram.ext.Updater
+token = 'token'
+bot = telebot.AsyncTeleBot(token)
+inits = None
+coefs = None
+cost = None
+willsolve = False
+studlist=None
+url='http://bettotaledu.tilda.ws/'
+soup=None
+@bot.message_handler(commands=['start', 'help'])
+def start_help_command(message):
+    bot.send_message(message.chat.id,
+                     'Приветствую. Я принимаю ставки по решению задач. Чтобы сделать ставку, нужно ввести фамилию и '
+                     'инициалы ученика. (пример - "Иванов И.И.")')
+    bot.register_next_step_handler(message, get_initials)
+
+def get_initials(message):
+    global inits, coefs, willsolve, srudlist
+    inits = message.text
+    coefs = db.get_coefs(message.chat.id)
+    page=requests.get(url)
+    assert page.status_code==200, 'ConnectionError'
+    soup=BeautifulSoup(page.text, 'html.parser')
+    tag=str(soup.find('div', class_='t431__data-part2').extract())
+    studlist = db.get_stud_list(tag)
+    while inits not in studlist:
+        bot.send_message(message.chat.id, 'Такого ученика нет. Нужно ввести имя ученика из таблицы.')
+        bot.register_next_step_handler(message, get_initials)
+    bot.send_message(message.chat.id,
+                     'Коэффициент на решение - ' + str(coefs[0]) + ';\nКоэффициент на нерешение - ' + str(
+                         coefs[1]) + '.')
+    bot.send_message(message.chat.id, 'Теперь нужно ввести сумму ставки (в рублях).')
+    bot.register_next_step_handler(message, get_cost)
+
+
+def get_cost(message):
+    global cost
+    cost = int(message.text)
+    bot.send_message(message.chat.id, 'На какой исход делается ставка? ("Решит"/"Не решит")')
+    bot.register_next_step_handler(message, get_event)
+
+
+def get_event(message):
+    global coefs, inits, cost, willsolve
+    text = message.text
+    time = db.get_solve_time(inits)
+    while text not in ['Решит', 'Не решит']:
+        bot.send_message(message.chat.id, 'Нужно ввести одну из двух фраз: "Решит" или "Не решит" (без кавычек).')
+        bot.register_next_step_handler(message, get_event)
+    if text == 'Решит':
+        willsolve = True
+    db.get_bet(inits, coefs[1 - int(willsolve)])
+    bot.send_message(message.chat.id, 'Ставка принята.')
+
+
+bot.polling(interval=0, none_stop=True)
